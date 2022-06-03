9e0dd4c8fe6f92feb2d034351300e4be5b8b3d73
singularis <singularis314@gmail.com>
2022-06-03 18:56:39 +0000

    todo

--- todov1/app.js
+++ todov1/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 const {
     reset
 } = require("nodemon");
@@ -7,32 +8,44 @@ const {
 
 
 const app = express();
-var items = [];
+const items = [];
+const workItems = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+app.use(express.static("public"))
 
 app.get("/", function (req, res) {
-    var options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    };
-    var today = new Date();
     res.render('list', {
-        kindOfDay: today.toLocaleDateString("en-US", options),
+        listTitle: date.getDate(),
         items: items
     });
 
 });
 
-app.post("/", function (req, res) {
-    items.push(req.body.nextItem);
-    res.redirect("/");
-});
+app.get("/work", function (req, res) {
+    res.render('list', {
+        listTitle: "work list",
+        items: workItems
+    });
+})
 
+app.get("/about", function (req, res) {
+    res.render('about', {
+
+    });
+})
+
+app.post("/", function (req, res) {
+    if (req.body.list == "work") {
+        workItems.push(req.body.nextItem);
+        res.redirect("/work");
+    } else {
+        items.push(req.body.nextItem);
+        res.redirect("/");
+    }
+});
 
 
 

2fed9f2ab72a520f50d9d951e35649a02c5124ac
Dante <singularis314@gmail.com>
2022-07-15 18:29:48 +0000

    Add mapping

--- keeper/src/Note.jsx
+++ keeper/src/Note.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-function Note() {
+function Note(props) {
   return (
     <div className="note">      
-        <h1>Keeper</h1>
-        <p>Content</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
   );
 }
